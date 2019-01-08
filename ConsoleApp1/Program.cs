07bd3eae80e21386be9e3ae87b188781e1fa07a2
Matteo Sorrentino <Ciro & Luisa@DESKTOP-1GLGU9U>
2019-01-08 16:07:28 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.MaxValue;
+
+            Console.WriteLine(num);
+
+            int num2 = Convert.ToInt16(num);
+
+            Console.WriteLine(num2);
+
+            Console.ReadLine();
+        }
+    }
+}
